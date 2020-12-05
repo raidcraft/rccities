f5e73911d3e80b7683a29645aa9bb1109e115ec0
Philip Urban <urban.philip@gmail.com>
2020-12-05 10:47:25 +0100

    fix: Fix command context

--- src/main/java/net/silthus/rccities/commands/CommandSetup.java
+++ src/main/java/net/silthus/rccities/commands/CommandSetup.java
@@ -74,31 +74,43 @@ public class CommandSetup {
     private void registerCityContext(PaperCommandManager commandManager) {
 
         commandManager.getCommandContexts().registerIssuerAwareContext(City.class, c -> {
-            String cityName = c.popFirstArg();
+            String cityName = c.getFirstArg(); // Do not pop argument here
 
-            City city;
-            if(Strings.isNullOrEmpty(cityName)) {
-                Plot plot = plugin.getPlotManager().getPlot(c.getPlayer().getLocation().getChunk());
-                if (plot == null) {
+            City city = null;
 
-                    List<Resident> citizenships = plugin.getResidentManager().getCitizenships(c.getPlayer().getUniqueId());
-                    if (1 != citizenships.size()) {
-                        throw new InvalidCommandArgument(
-                                "Hier befindet sich keine Stadt oder du bist Einwohner in mehr als einer Stadt!");
-                    }
+            // At first try to get city by name
+            //---------------------------------
+            if(!Strings.isNullOrEmpty(cityName)) {
+                city = plugin.getCityManager().getCity(cityName);
+                if(city != null) {
+                    c.popFirstArg(); // Pop argument from stack if city was found
+                }
+            }
+
+            // If city still not set, try to get city
+            // of player if there is only one citizenship
+            //-------------------------------------------
+            if(city == null) {
+                List<Resident> citizenships = plugin.getResidentManager().getCitizenships(c.getPlayer().getUniqueId());
+                if (1 == citizenships.size()) {
                     city = citizenships.get(0).getCity();
-                } else {
+                }
+            }
+
+            // If still no city found, try to get city
+            // at current location
+            //----------------------------------------
+            if(city == null) {
+                Plot plot = plugin.getPlotManager().getPlot(c.getPlayer().getLocation().getChunk());
+                if(plot != null) {
                     city = plot.getCity();
                 }
-                if (city == null) {
-                    throw new InvalidCommandArgument(
-                            "Es ist ein Fehler aufgetreten. Gebe den Stadtnamen direkt an!");
-                }
-            } else {
-                city = plugin.getCityManager().getCity(cityName);
-                if (city == null) {
-                    throw new InvalidCommandArgument("Es gibt keine Stadt mit diesem Namen!");
-                }
+            }
+
+            // Throw exception if no city was found
+            //-------------------------------------
+            if(city == null) {
+                throw new InvalidCommandArgument("Bitte gebe eine Stadt an!");
             }
 
             return city;
@@ -108,23 +120,30 @@ public class CommandSetup {
     private void registerPlotContext(PaperCommandManager commandManager) {
 
         commandManager.getCommandContexts().registerIssuerAwareContext(Plot.class, c -> {
-            String plotRegionName = c.popFirstArg();
+            String plotRegionName = c.getFirstArg(); // Do not pop argument here
 
-            Plot plot;
-            if(Strings.isNullOrEmpty(plotRegionName)) {
+            Plot plot = null;
+
+            // At first try to get plot by name
+            //---------------------------------
+            if(!Strings.isNullOrEmpty(plotRegionName)) {
+                plot = plugin.getPlotManager().getPlot(plotRegionName);
+                if(plot != null) {
+                    c.popFirstArg(); // Pop argument from stack if plot was found
+                }
+            }
+
+            // If no plot was found, try to get
+            // plot at current location
+            //---------------------------------
+            if(plot == null) {
                 plot = plugin.getPlotManager().getPlot(c.getPlayer().getLocation().getChunk());
-                if (plot == null) {
-                    throw new ConditionFailedException("Hier befindet sich kein Plot!");
-                }
-            } else {
-                try {
-                    plot = plugin.getPlotManager().getPlot(plotRegionName);
-                } catch (IllegalArgumentException e) {
-                    plot = null;
-                }
-                if (plot == null) {
-                    throw new InvalidCommandArgument("Es gibt kein Plot mit diesem Namen!");
-                }
+            }
+
+            // Throw exception if no plot was found
+            //-------------------------------------
+            if(plot == null) {
+                throw new InvalidCommandArgument("Bitte gebe einen Plot an!");
             }
 
             return plot;
